0ff1ea4c2350f2918d0f73339267bfef764bd9c4
MeysamSMH7 <meysamsm@yahoo.com>
2018-10-16 14:47:57 +0330

    Initial commit

---
+++ app/src/main/java/com/pol/poleuser/Activity_mainpol_user.java
@@ -0,0 +1,13 @@
+package com.pol.poleuser;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_mainpol_user extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mainpol_user);
+    }
+}
