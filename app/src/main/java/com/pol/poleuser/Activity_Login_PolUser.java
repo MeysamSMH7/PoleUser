b3dc6aa23a226daeeb9635453ccb1f0e07b4de68
MeysamSMH7 <meysamsm@yahoo.com>
2018-10-30 14:30:01 +0330

    30-oct 14:29

--- app/src/main/java/com/pol/poleuser/Activity_Login_PolUser.java
+++ app/src/main/java/com/pol/poleuser/Activity_Login_PolUser.java
@@ -256,7 +256,7 @@ public class Activity_Login_PolUser extends AppCompatActivity {
             LinearRegForgetPass.setVisibility(View.GONE);
 
             //Spinner for Register
-            SpinnerClass spinner = new SpinnerClass(Activity_Login_PolUser.this, spnState, spnCity);
+            SpinnerClass spinner = new SpinnerClass(Activity_Login_PolUser.this, spnState, spnCity,false);
             spinner.spinner();
 
         } else if (countbtnClick < 5) {
